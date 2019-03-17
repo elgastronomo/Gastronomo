c6600c259d747eafb2efa5147e23efa3b9169f0c
roberto <roberto@DESKTOP-QH37B0P>
2019-03-17 12:25:03 +0100

    loggin

--- src/main/java/es/ucm/fdi/iw/LoginSuccessHandler.java
+++ src/main/java/es/ucm/fdi/iw/LoginSuccessHandler.java
@@ -46,12 +46,12 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 	    
 	    // add a 'u' session variable, accessible from thymeleaf via ${session.u}
 	    log.info("Storing user info for {} in session {}", login, session.getId());
-		User u = entityManager.createNamedQuery("User.ByLogin", User.class)
+		User user = entityManager.createNamedQuery("User.ByLogin", User.class)
 		        .setParameter("userLogin", login)
 		        .getSingleResult();			   	
-		session.setAttribute("u", u);
+		session.setAttribute("user", user);
 		
 		// redirects to 'admin' or 'user/{id}', depending on the user
-		response.sendRedirect(u.hasRole("ADMIN") ? "admin" : "user/" + u.getId());
+		response.sendRedirect("");
 	}
 }
